fe5b1e2b564c56b184cc501bff391dba8cac11da
Daniil <danilsimonov491@gmail.com>
2026-06-25 01:33:06 +0200

    Add task priorities and status toggle

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
-    status TEXT
+    status TEXT,
+    priority TEXT
 );
 """)
 
@@ -21,15 +22,28 @@ def show_tasks():
         return
     
     for row in rows:
-        print(f"ID: {row[0]} | Task: {row[1]} | Status: {row[2]}")
+        print(f"ID: {row[0]} | Task: {row[1]} | Status: {row[2]} | Priority: {row[3]}")
 
 
 def add_task():
     user_task = input("What task do you want to add? Write a new task: ")
     cursor.execute("""
-    INSERT INTO tasks (title, status)
-    VALUES (?, 'open');
+    INSERT INTO tasks (title, status, priority)
+    VALUES (?, 'open', 'low');
     """, (user_task,))
+    conn.commit()
+
+
+def update_priority():
+    task_id = int(input("Task ID: "))
+    priority = input("low/medium/high: ")
+
+    cursor.execute("""
+    UPDATE tasks
+    SET priority = ?
+    WHERE id = ?
+    """, (priority, task_id))
+
     conn.commit()
 
 
@@ -38,17 +52,26 @@ def update_task():
         user_id = int(input("Which task do you want to update? Choose the task ID: "))
         cursor.execute("SELECT * FROM tasks WHERE id = ?", (user_id,))
         task = cursor.fetchone()
+        
+        if not task:
+            print("Task not found")
+            return
 
-        if task:
+        elif task[2] == 'open':
             cursor.execute("""
             UPDATE tasks
             SET status = 'done'
             WHERE id = ?;
             """, (user_id,))
             conn.commit()
-
-        else:
-            print("Task not found")
+        
+        elif task[2] == 'done':
+            cursor.execute("""
+            UPDATE tasks
+            SET status = 'open'
+            WHERE id = ?;
+            """, (user_id,))
+            conn.commit()
 
     except ValueError:
         print("Please enter a valid number (task ID)")
@@ -73,6 +96,7 @@ def delete_task():
     except ValueError:
         print("Please enter a valid number (task ID)")
 
+
 def filter_tasks():
     status = input("Filter by status: (open / done): ").strip().lower()
 
@@ -84,10 +108,11 @@ def filter_tasks():
     rows = cursor.fetchall()
 
     for row in rows:
-        print(f"ID: {row[0]} | Task: {row[1]} | Status: {row[2]}")
+        print(f"ID: {row[0]} | Task: {row[1]} | Status: {row[2]} | Priority: {row[3]}")
+
 
 def menu():
-    menu_options = ["1. Add task", "2. Show tasks", "3. Update task", "4. Delete task", "5. Filter tasks", "6. Exit"]
+    menu_options = ["1. Add task", "2. Show tasks", "3. Update task", "4. Delete task", "5. Filter tasks", "6. Update priority", "7. Exit"]
     for menu_option in menu_options:
         print(f"{menu_option}")
 
@@ -98,7 +123,7 @@ def main():
 
         if choice == "1":
             add_task()
-            print("Task added successfully")
+            print("Task added successfully.")
             
         elif choice == "2":
             show_tasks()
@@ -106,18 +131,24 @@ def main():
 
         elif choice == "3":
             update_task()
-            print("Task updated successfully")
+            print("Task updated successfully.")
 
         elif choice == "4":
             delete_task()
-            print("Task deleted successfully")
+            print("Task deleted successfully.")
 
         elif choice == "5":
             filter_tasks()
             input("Press Enter to continue...")
 
         elif choice == "6":
+            update_priority()
+            print("Priority updated successfully.")
+
+        elif choice == "7":
             break
 
         else:
             print("Invalid option") 
+            
+main()
